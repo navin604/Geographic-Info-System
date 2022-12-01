9599ad056c8e11541399987e0ce704354dd73932
navin604 <55507003+navin604@users.noreply.github.com>
2022-12-01 14:13:46 -0800

    Hashtable base template

---
+++ HashTable.h
@@ -0,0 +1,20 @@
+//
+// Created by navin on 2022-12-01.
+//
+#ifndef CODE_HASHTABLE_H
+#define CODE_HASHTABLE_H
+
+class HashTable
+{
+private:
+
+
+public:
+
+
+
+};
+
+
+
+#endif //CODE_HASHTABLE_H
