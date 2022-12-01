7d66eea3bfaa7e60105ee700256710c7be8b1c51
navin604 <55507003+navin604@users.noreply.github.com>
2022-12-01 10:44:31 -0800

    Implemented LRU/MRU Buffer Pool

---
+++ BufferPool.h
@@ -0,0 +1,55 @@
+//
+// Created by Navin on 2022-11-30.
+//
+#ifndef CODE_BUFFERPOOL_H
+#define CODE_BUFFERPOOL_H
+#include <iostream>
+#include <vector>
+
+
+class BufferPool
+{
+private:
+    int MAX_SIZE = 15;
+    std::vector<std::string> pool;
+
+public:
+    BufferPool();
+    int getMaxSize();
+    void addElement(std::string);
+    std::string output();
+
+
+};
+
+// BufferPool constructor
+BufferPool::BufferPool() = default;
+
+int BufferPool::getMaxSize() {
+    return MAX_SIZE;
+
+};
+
+void BufferPool::addElement(std::string x) {
+    if (pool.size() == getMaxSize()) {
+        pool.erase(pool.begin());
+
+    }
+
+    pool.push_back(x);
+}
+
+std::string BufferPool::output() {
+    std::ostringstream os;
+    os << "MRU\n";
+    for (int i = pool.size()-1; i >= 0; i--) {
+        os <<"    "<<pool[i] << "\n";
+
+    }
+    os << "LRU\n";
+    os << std::endl;
+    return os.str();
+
+}
+
+#endif //CODE_BUFFERPOOL_H
