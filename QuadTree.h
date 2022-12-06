e6916cae526a7c4fee24b31e0c5e95f9d7cf1206
navin604 <55507003+navin604@users.noreply.github.com>
2022-12-05 18:02:53 -0800

    Implemented search

--- QuadTree.h
+++ QuadTree.h
@@ -33,18 +33,57 @@ public:
         root.cell = "Empty";
 
     }
+    void setVal(int westLong, int eastLong, int southLat, int northLat) {
+        root.sector = {westLong,southLat,eastLong,northLat};
+
+
+    }
     void init(int xcoord, int ycoord, std::vector<int> offset) {
         insert(xcoord,ycoord,offset,&root);
 
 
     }
+
+    std::vector<int> search(int xcoord, int ycoord) {
+        point temp_root = root;
+
+
+        while (true)
+        {
+            if (temp_root.cell == "Empty") return {};
+            if (temp_root.cell == "Leaf")
+            {
+                if (xcoord == temp_root.coord_x && ycoord== temp_root.coord_y)
+                    return temp_root.offsets;
+                return {};
+            }
+            while (temp_root.cell == "QUAD");  {
+                int count;
+                for (count = 0; count < QUAD; count++)
+                {
+                    //change into 4 variables
+                    std::vector<int> sector = temp_root.subtree[count].sector;
+                    if (xcoord >= sector[0] && ycoord >= sector[1])
+                        if (xcoord <= sector[2] && ycoord <= sector[3])
+                        {
+                            temp_root= (point)temp_root.subtree[count];
+                            break;
+                        }
+                }
+                if (count == 4) return{};
+
+            }
+        }
+
+    }
+
     void insert(int xcoord, int ycoord, std::vector<int> offset, point *p) {
         while (true){
             //Defines parent Node
             if (p->cell == "Empty"){
-                std::cout << "empty\n";
 
-                if ((xcoord >= p->sector[0] and xcoord <= p->sector[2]) and (ycoord >= p->sector[1] and ycoord <= p->sector[3])) {
+
+                if ((xcoord >= p->sector[0] && xcoord <= p->sector[2]) && (ycoord >= p->sector[1] && ycoord <= p->sector[3])) {
                     //Sets Coordinates
                     p->coord_x = xcoord;
                     p->coord_y = ycoord;
@@ -61,13 +100,12 @@ public:
             }
             //Locates correct child node
             if (p->cell =="Quad") {
-                std::cout << "quads\n";
 
                 int cnt;
                 //Checks each child of current Point
                 for(cnt = 0; cnt<::QUAD;cnt++){
                     point *curr = &(p->subtree[cnt]);
-                    if ((xcoord >= curr->sector[0] and xcoord <= curr->sector[2]) and (ycoord >= curr->sector[1] and ycoord <= curr->sector[3])) {
+                    if ((xcoord >= curr->sector[0] && xcoord <= curr->sector[2]) && (ycoord >= curr->sector[1] && ycoord <= curr->sector[3])) {
                         p = curr;
                         break;
                     }
@@ -77,8 +115,7 @@ public:
             }
             //Adds offset to leaf or splits leaf into 4 children
             if (p->cell =="Leaf") {
-                //std::cout << "leaf\n";
-                if (xcoord == p->coord_x and ycoord == p->coord_y) {
+                if (xcoord == p->coord_x && ycoord == p->coord_y) {
 
                     for (int i = 0; i<offset.size();i++) {
                         p->offsets.push_back(offset[i]);
@@ -88,22 +125,18 @@ public:
                 else{
                     point child;
                     child.cell = "Empty";
-                    std::cout << "wdadawdawdawda\n";
+                    //Calculates new regions
                     child.sector = {p->sector[0],p->sector[1],p->sector[0] +(p->sector[2]-p->sector[0])/2,p->sector[1]+(p->sector[3]-p->sector[1])/2};
                     p->subtree.push_back(child);
-
                     child.sector = {p->sector[0] +(p->sector[2]-p->sector[0])/2,p->sector[1],p->sector[2],p->sector[1]+(p->sector[3]-p->sector[1])/2};
                     p->subtree.push_back(child);
-
                     child.sector = {p->sector[0],p->sector[1]+(p->sector[3]-p->sector[1])/2,p->sector[0] +(p->sector[2]-p->sector[0])/2,p->sector[3]};
                     p->subtree.push_back(child);
-
                     child.sector = {p->sector[0] +(p->sector[2]-p->sector[0])/2,p->sector[1]+(p->sector[3]-p->sector[1])/2,p->sector[2],p->sector[3]};
                     p->subtree.push_back(child);
 
-                    std::cout << p->cell;
                     p->cell = "Quad";
-                    std::cout << p->cell;
+
                     insert(p->coord_x,p->coord_y,p->offsets,p);
 
                 }
@@ -114,7 +147,11 @@ public:
     }
 
 
+
+
 };
+
+
 
 
 #endif //CODE_QUADTREE_H
