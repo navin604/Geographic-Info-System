0fb28ee34efb0c7dd86fc2e459a3c2ca4062e43c
navin604 <55507003+navin604@users.noreply.github.com>
2022-12-07 18:15:22 -0800

    Clean up

--- BufferPool.h
+++ BufferPool.h
@@ -12,13 +12,13 @@
 class BufferPool
 {
 private:
+    //Holds a record and its offset
     struct poolItem {
         std::string offset;
         std::string record;
     };
     int MAX_SIZE = 15;
     std::vector<poolItem> pool;
-
 public:
     BufferPool();
     int getMaxSize();
@@ -26,65 +26,57 @@ public:
     std::string print();
     std::string search(std::string,char* database);
     std::string filter_search(std::string,char* database,std::string filter);
-
-
-
 };
 
 // BufferPool constructor
 BufferPool::BufferPool() = default;
 
 int BufferPool::getMaxSize() {
+    //Returns max amount of records the pool can hold
     return MAX_SIZE;
 
 }
 
 void BufferPool::addElement(std::string line, std::string offset) {
-
     if (pool.size() == getMaxSize()) {
-        //std::cout<< "Deleteing" << pool[0].record<< "\n";
+        //If pool is full, remove LRU element(least recently used)
         pool.erase(pool.begin());
 
     }
-
+    //Inserts record
     poolItem x;
     x.record = line;
     x.offset = offset;
-    //std::cout<< "Inserting" << x.record;
     pool.push_back(x);
 }
 
-
 std::string BufferPool::print() {
+    //Prints content of pool for debug command
     std::ostringstream os;
     if (pool.size() <=0) {
         os << "EMPTY POOL\n";
         os << std::endl;
         return os.str();
-
     }
     os << "MRU\n";
     for (int i = pool.size()-1; i >= 0; i--) {
         os <<"    "<<pool[i].offset << ": "<<pool[i].record << "\n";
-
     }
     os << "LRU\n";
     os << std::endl;
     return os.str();
-
 }
 
-
-
 std::string BufferPool::search(std::string offset,char* database) {
+    //Checks if record exists in pool, if so, remove and reinsert as MRU
     for (int i = 0; i < pool.size(); i++) {
         if (pool[i].offset == offset) {
             pool.push_back(pool[i]);
             pool.erase(pool.begin()+i);
             return pool[pool.size()-1].record;
         }
-
     }
+    //Record doesnt exist, retrieve from database file
     int x = stoi(offset);
     std::string line;
     std::ifstream myfile(database);
@@ -94,13 +86,10 @@ std::string BufferPool::search(std::string offset,char* database) {
             return line;
         }
     }
-
 }
 
-
-
-
 std::string BufferPool::filter_search(std::string offset,char* database, std::string filter) {
+    //Searches for record based on filter
     for (int i = 0; i < pool.size(); i++) {
         if (pool[i].offset == offset) {
             std::vector<std::string> record = GISRecord::convert(pool[i].record);
@@ -112,7 +101,7 @@ std::string BufferPool::filter_search(std::string offset,char* database, std::st
             return "";
         }
     }
-
+    //File not in pool, retrieves from database if filter matches
     int x = stoi(offset);
     std::string line;
     std::ifstream myfile(database);
@@ -123,14 +112,9 @@ std::string BufferPool::filter_search(std::string offset,char* database, std::st
                 addElement(line, offset);
                 return line;
             }
-
             return "";
         }
     }
-
-
-
 }
-
 
 #endif //CODE_BUFFERPOOL_H
